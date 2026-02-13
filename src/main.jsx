c6fe9e2bcc09e6b1bffc9d0834b05a6fa50ea1e6
Adnan22332 <adnanshoukat5262902@gmail.com>
2026-02-13 19:21:36 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
 import './index.css'
+import { injectSpeedInsights } from "@vercel/speed-insights"
+
+injectSpeedInsights()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
